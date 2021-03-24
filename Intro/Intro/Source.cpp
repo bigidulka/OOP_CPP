c7f0142abb37750634321fbe6bf9d3dca08eddd3
Bogdan <udincevb@gmail.com>
2021-03-24 21:55:43 +0300

    Constructors

--- Intro/Intro/Source.cpp
+++ Intro/Intro/Source.cpp
@@ -21,19 +21,43 @@ public:
 	{
 		this -> y = y;
 	}
+	//constructors
+	Point()
+	{
+		x = y = 0;
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+	//methdos
+	void Print()const
+	{
+		cout << "X = " << x << '\t' << "Y = " << y << endl;
+	}
 };
-void func(Point* pA)
+void func(Point* p)
 {
-	cout << pA;
+	cout << p->get_x() << '\t' << p->get_y() << endl;
 }
 // Создавая структуру или класс мы создаем новый тип данных
 		// CLASS - это тип данных
 		// STRUCT - это тип данных
 //Обьекты класса или структуры являются переменными соответствующего типа
 
+//#define intro
+
 void main()
 {
 	setlocale(0, " ");
+#ifdef intro
 	int a;		// Объявляем перемненную a типа int
 	Point A;	// Обьявляем переменную А типа Point
 				// Создаем обьект А структуры Point
@@ -46,5 +70,17 @@ void main()
 	cout << pA->get_x() << '\t' << pA->get_y() << endl;
 	cout << sizeof(A) << endl;
 	cout << sizeof(pA) << endl;
+	{
+		//Безымяная область видимости внутри функции main
+		//class obj;
+	}
+	//Здесь объекта obj уже не будет
 	//cout << (*pA).x << '\t' << (*pA).x << endl;
+#endif // intro
+	Point A;	
+	A.Print();
+//#define razd "\n - - - - - - - - - - - \n"
+	//cout << razd << endl;
+	Point B(2, 3);
+	B.Print();
 }
